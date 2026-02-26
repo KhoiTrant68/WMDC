aaeaf18f4d848828dd9ed9be6c1c3e3e8cd901ed
KhoiTrant <85048572+KhoiTrant68@users.noreply.github.com>
2026-02-26 10:51:11 +0700

    Update

--- eval.py
+++ eval.py
@@ -53,7 +53,7 @@ def main():
 
     device = "cuda" if args.cuda and torch.cuda.is_available() else "cpu"
 
-    print(f"Loading model from {args.checkpoint}...")
+    print(f"Loading model from {args.checkpoint}... to {device}")
     model = WMDC(N=args.N, M=args.M, num_slices=5).to(device)
     
     checkpoint = torch.load(args.checkpoint, map_location=device, weights_only=False)
