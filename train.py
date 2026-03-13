f5e9025374623f4b28e533ac29f0e85b6a7f7873
KhoiTrant <85048572+KhoiTrant68@users.noreply.github.com>
2026-03-13 11:46:55 +0700

    Update

--- train.py
+++ train.py
@@ -182,7 +182,7 @@ def train_one_epoch(
         aux_loss_meter.update(aux_loss_val)
         bpp_meter.update(bpp_val)
 
-        pbar.set_postfix(f"Train Epoch: {epoch}[{i}/{len(train_dataloader)}] Loss: {loss_val:.4f} | Aux_Loss: {aux_loss_val:.4f}  | Bpp: {bpp_val:.4f})
+        pbar.set_postfix(f"Train Epoch: {epoch}[{i}/{len(train_dataloader)}] Loss: {loss_val:.4f} | Aux_Loss: {aux_loss_val:.4f}  | Bpp: {bpp_val:.4f}")
 
         if accelerator.is_main_process and i % 100 == 0:
             if writer:
