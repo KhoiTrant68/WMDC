d86cc9b04f87807dcba6b7f3d54a65abf9fcee0b
KhoiTrant <khoiminhtran681@gmail.com>
2026-04-12 13:57:40 +0700

    Update learning rate milestones in argument parser

--- train.py
+++ train.py
@@ -467,7 +467,7 @@ def parse_args():
     p.add_argument("--disp-weight", type=float, default=0.1)
     p.add_argument("--ot-eps", type=float, default=0.1)
     p.add_argument("--sinkhorn-iters", type=int, default=20)
-    p.add_argument("--lr-milestones", type=int, nargs="+", default=[60, 85])
+    p.add_argument("--lr-milestones", type=int, nargs="+", default=[3, 4])
     p.add_argument("--lr-gamma", type=float, default=0.1)
     p.add_argument(
         "--gap-check-interval",
