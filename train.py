2e0f046578b25dda8c7ad335d77acfe15c4509da
KhoiTrant <85048572+KhoiTrant68@users.noreply.github.com>
2026-05-21 06:58:25 +0000

    Update

--- train.py
+++ train.py
@@ -949,12 +949,15 @@ def main():
         raise ValueError(
             f"--patch-size must be a multiple of 64, got {args.patch_size}."
         )
-    find_unused = (
-        args.column_entropy_weight == 0.0
-        or args.row_entropy_weight == 0.0
-        or args.alignment_weight == 0.0
+    # find_unused = (
+    #     args.column_entropy_weight == 0.0
+    #     or args.row_entropy_weight == 0.0
+    #     or args.alignment_weight == 0.0
+    # )
+    ddp_kwargs = DistributedDataParallelKwargs(
+        find_unused_parameters=True,
+        gradient_as_bucket_view=False,
     )
-    ddp_kwargs = DistributedDataParallelKwargs(find_unused_parameters=find_unused)
     accelerator = Accelerator(kwargs_handlers=[ddp_kwargs], mixed_precision="no")
 
     save_dir = os.path.join(args.save_path, f"lambda_{args.lmbda}_{args.metric}")
